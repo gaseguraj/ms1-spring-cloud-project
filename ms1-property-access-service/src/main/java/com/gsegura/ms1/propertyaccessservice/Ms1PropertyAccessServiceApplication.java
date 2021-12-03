833dd266be5386675248d255865dd350ea53fc32
German Segura <german.andres.segura@gmail.com>
2021-12-02 23:02:39 -0500

    property access service

---
+++ ms1-property-access-service/src/main/java/com/gsegura/ms1/propertyaccessservice/Ms1PropertyAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gsegura.ms1.propertyaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms1PropertyAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms1PropertyAccessServiceApplication.class, args);
+    }
+
+}
---
+++ ms1-property-access-service/src/test/java/com/gsegura/ms1/propertyaccessservice/Ms1PropertyAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsegura.ms1.propertyaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1PropertyAccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
