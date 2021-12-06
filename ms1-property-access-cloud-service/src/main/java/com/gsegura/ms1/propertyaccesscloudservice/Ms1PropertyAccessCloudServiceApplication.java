c9729f36a4affe0d5df29f1742000c4f452503c5
German Segura <german.andres.segura@gmail.com>
2021-12-06 15:24:55 -0500

    ms1-property-access-service

---
+++ ms1-property-access-cloud-service/src/main/java/com/gsegura/ms1/propertyaccesscloudservice/Ms1PropertyAccessCloudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gsegura.ms1.propertyaccesscloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms1PropertyAccessCloudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms1PropertyAccessCloudServiceApplication.class, args);
+    }
+
+}
---
+++ ms1-property-access-cloud-service/src/test/java/com/gsegura/ms1/propertyaccesscloudservice/Ms1PropertyAccessCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsegura.ms1.propertyaccesscloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1PropertyAccessCloudServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
